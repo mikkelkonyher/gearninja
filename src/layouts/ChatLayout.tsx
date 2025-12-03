1bbeb5b5dc23f57fa40d334a6799afed44168f5b
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-12-03 09:36:17 +0100

    Update ChatLayout.tsx

--- src/layouts/ChatLayout.tsx
+++ src/layouts/ChatLayout.tsx
@@ -29,7 +29,7 @@ export function ChatLayout() {
             </div>
             <h3 className="text-xl font-semibold text-white mb-2">Vælg en samtale</h3>
             <p className="max-w-sm">
-              Vælg en samtale fra listen til venstre for at se beskeder og chatte med sælgere.
+              Vælg en samtale fra listen til venstre for at se beskeder og chatte med købere eller sælgere.
             </p>
           </div>
         )}
